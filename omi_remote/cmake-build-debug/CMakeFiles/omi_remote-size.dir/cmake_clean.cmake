000ca24da8adbaabc7221cb68bc8209df07bb57f
NikolayDupak <37293757+NikolayDupak@users.noreply.github.com>
2021-03-02 23:59:12 +0300

    Delete omi_remote/cmake-build-debug directory

--- omi_remote/cmake-build-debug/CMakeFiles/omi_remote-size.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/omi_remote-size"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/omi_remote-size.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- omi_remote/cmake-build-debug/CMakeFiles/omi_remote-upload.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/omi_remote-upload"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/omi_remote-upload.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
